2f8acc78294211e4c5c5713d8a5b61445d9b95e4
aadarsh8434 <122546575+aadarsh8434@users.noreply.github.com>
2024-06-09 22:54:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,68 @@
-async function getWeather() {
-    const city = document.getElementById('city').value;
-    const apiKey = 'YOUR_API_KEY'; // Replace with your OpenWeatherMap API key
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let cityEl = document.querySelector(".city");
 
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
+let iconEl = document.querySelector(".icon");
 
-        if (data.cod === 200) {
-            const weatherResult = `
-                <h2>${data.name}</h2>
-                <p>Temperature: ${data.main.temp} °C</p>
-                <p>Weather: ${data.weather[0].description}</p>
-                <p>Humidity: ${data.main.humidity}%</p>
-                <p>Wind Speed: ${data.wind.speed} m/s</p>
-            `;
-            document.getElementById('weather-result').innerHTML = weatherResult;
-        } else {
-            document.getElementById('weather-result').innerHTML = `<p>${data.message}</p>`;
-        }
-    } catch (error) {
-        document.getElementById('weather-result').innerHTML = `<p>Unable to fetch weather data.</p>`;
-    }
-}
+let descriptionEl = document.querySelector(".description");
+
+let temperatureEl = document.querySelector(".temp");
+
+let humidityEl = document.querySelector(".humidity");
+
+let windEl = document.querySelector(".wind");
+
+let searchBar = document.querySelector(".search-bar");
+
+let searchEl = document.querySelector(".search button");
+
+let weatherEl = document.querySelector(".weather");
+
+let weather = {
+ "apikey": "a6f6fef1470f473cb0694459230605",
+
+ fetchWeather: function (city) {
+  fetch("http://api.weatherapi.com/v1/current.json?key=a6f6fef1470f473cb0694459230605%20&q=" + city + "&aqi=no").then((response) => response.json()).then((data) => this.displayWeather(data));
+ },
+
+ displayWeather: function (data) {
+  const { name } = data.location;
+
+  const { icon, text } = data.current.condition;
+
+  const { temp_c, humidity } = data.current;
+
+  const { wind_kph } = data.current;
+
+  cityEl.innerText = `Weather in ${name}`;
+
+  iconEl.src = icon;
+
+  descriptionEl.innerText = text;
+
+  temperatureEl.innerText = `Temperature: ${temp_c}°C`;
+
+  humidityEl.innerText = `Humidity: ${humidity}%`;
+
+  windEl.innerText = `Wind Speed: ${wind_kph} km/hr`;
+
+  weatherEl.classList.remove("loading");
+
+  document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+ },
+
+ search: function () {
+  this.fetchWeather(searchBar.value);
+ }
+};
+
+searchEl.addEventListener("click", () => {
+ console.log("Clicked!");
+ weather.search();
+});
+
+searchBar.addEventListener("keyup", (event) => {
+ if (event.key === "Enter") {
+  weather.search();
+ }
+});
+
+weather.fetchWeather("Lagos");
